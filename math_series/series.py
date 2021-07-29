4e2dd2938e0e27c84f5a4687514f847385fe2a1a
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-07-29 16:43:32 +0300

    Update series.py

--- math_series/series.py
+++ math_series/series.py
@@ -1,33 +1,36 @@
 def fibonacci(n):
-    if n == 0:
-        return 0
+    # if n == 0:
+    #     return 0
         
-    elif n == 1:
-        return 1
+    # elif n == 1:
+    #     return 1
 
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-
-def lucas(m):
-    if m == 0:
-        return 2
-
-    elif m == 1:
-        return 1
-
-    else:
-        return lucas(m-1) + lucas(m-2)
+    # else:
+    #     return fibonacci(n-1) + fibonacci(n-2)
+    return sum_series(n)
 
 
-def sum_series(z, x=0, y=1):
-    if z == 0:
+def lucas(n):
+    # if m == 0:
+    #     return 2
+
+    # elif m == 1:
+    #     return 1
+
+    # else:
+    #     return lucas(m-1) + lucas(m-2)
+    return sum_series(n,2,1)
+
+
+def sum_series(n,x=0,y=1):
+    if n == 0:
         return x
 
-    elif z == 1:
+    elif n == 1:
         return y
 
     else:
-        return sum_series(z-1, x, y) + sum_series(z-2, x, y)
+        return sum_series(n-1,x,y) + sum_series(n-2,x,y)
 
+print(fibonacci(6))
 print(lucas(6))
